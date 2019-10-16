b37b467b7cabdd92b77a817dba1a3eaa6ac80c3c
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-16 13:10:25 +0200

    Add project files.

---
+++ Convienza/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Convienza
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("quanti soldi devi spendere?");
+            double a = int.Parse(Console.ReadLine());
+            if(a<=500)
+            {
+                Console.WriteLine("conviene fare la spesa nel primo negozio");
+            }
+            else
+            {
+                Console.WriteLine("conviene fare la spesa nel secondo negozio");
+            }
+            Console.ReadLine();
+        }
+    }
+}
